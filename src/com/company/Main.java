f3195f0551646a370707fb8c7746895dcccb79dc
Nj <patelniraj00001@gmail.com>
2020-12-29 22:27:17 +0530

    task1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Additional");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first value");
+        int a= sc.nextInt();
+        System.out.println("enter the second value");
+        int b= sc.nextInt();
+        int sum = a + b;
+        System.out.println(" Total is that  " + sum + ".");
+
     }
 }
